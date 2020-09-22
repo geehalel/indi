d61f12a29646fe44b5753daf826cc1895d9f2480
geehalel <geehalel@gmail.com>
2020-09-22 12:30:27 +0200

    indi-eqmod: eqsnap1.py uses now select.poll to catch shutter port release

--- 3rdparty/indi-eqmod/eqsnap1.py
+++ 3rdparty/indi-eqmod/eqsnap1.py
@@ -2,21 +2,35 @@
 # -*- coding: utf8 -*-
 
 import os
+import stat
 import select
+import time
 
 PIPE_FILE='/tmp/eqsnapport1'
-os.mkfifo(PIPE_FILE)
+try:
+    s=os.stat(PIPE_FILE)
+    if not stat.S_ISFIFO(s.st_mode):
+        import sys
+        print("eqsnapport1: " + PIPE_FILE + " exists and is NOT a FIFO.")
+        print("Please change PIPE_FILE in this script. Aborting.")
+        sys.exit(1) 
+except FileNotFoundError:
+    os.mkfifo(PIPE_FILE)
 
+poll = select.poll()
 try:
     while True:
         f=open(PIPE_FILE, 'r')
         os.system("indi_setprop -s 'EQMod Mount.SNAPPORT1.SNAPPORT1_ON=On;SNAPPORT1_OFF=Off'")
-        rlist, wlist, xlist=[f], [], []
-        rready, wready, eready = select.select(rlist, wlist, xlist)
+        #print(time.asctime() + ": indi_setprop -s 'EQMod Mount.SNAPPORT1.SNAPPORT1_ON=On;SNAPPORT1_OFF=Off'")
+        #rlist, wlist, xlist=[f], [], []
+        #rready, wready, eready = select.select(rlist, wlist, xlist)
+        poll.register(f, select.POLLHUP)
+        poll.poll()
+        poll.unregister(f)
         os.system("indi_setprop -s 'EQMod Mount.SNAPPORT1.SNAPPORT1_ON=Off;SNAPPORT1_OFF=On'")
+        #print(time.asctime() + ": indi_setprop -s 'EQMod Mount.SNAPPORT1.SNAPPORT1_ON=Off;SNAPPORT1_OFF=On'")
 except KeyboardInterrupt:
     pass
 
 os.unlink(PIPE_FILE)
-
-        
